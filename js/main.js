4698248ad3cad75761f366d9bc1eec731bf76548
sydung22 <72313449+sydung22@users.noreply.github.com>
2021-09-13 23:48:56 -0600

    add

--- js/main.js
+++ js/main.js
@@ -4,4 +4,21 @@ $( document ).ready(function() {
         let windowPosition = window.scrollY > 73;
         header.classList.toggle('scrolling-active', windowPosition);
       })
+
+    $(".col-places").slice(0,5).show()
+    $("#load-more").on('click', function(e){
+        e.preventDefault();
+        $(".col-places:hidden").slice(0, 3).slideDown()
+        if($(".col-places:hidden").length == 0){
+          $("#load-more").fadeOut('slow')
+        }
+    })   
+    $(".col-product").slice(0,4).show()
+    $("#product-more").on('click', function(e){
+        e.preventDefault();
+        $(".col-product:hidden").slice(0, 2).slideDown()
+        if($(".col-product:hidden").length == 0){
+          $("#product-more").fadeOut('slow')
+        }
+    })
 });
